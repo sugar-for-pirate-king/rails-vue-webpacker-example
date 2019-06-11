b8539f10bc7b99e03279a0003c5c7b3ff5753bb2
philiplambok <philip.geraldi@harukaedu.com>
2019-06-11 10:05:10 +0700

    add dashboard component and vue component

---
+++ app/javascript/app-dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app-dashboard">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: "this is come from app dashboard-component"
+    };
+  }
+};
+</script>
+
+<style>
+p {
+  color: darkblue;
+}
+</style>
+
---
+++ app/javascript/app-home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app-home">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: "this is from app-home component"
+    };
+  }
+};
+</script>
+
+<style>
+p {
+  color: deeppink;
+}
+</style>
+
+
